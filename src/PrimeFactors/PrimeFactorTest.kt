5d9af584d45125fd9571d6b1eda510ebc4fb3890
Sirapat Siribandit <sirapat.siribandit@agoda.com>
2019-03-09 17:35:37 +0700

    Init project

---
+++ src/PrimeFactors/PrimeFactorTest.kt
@@ -0,0 +1,34 @@
+package PrimeFactors
+
+import org.junit.Assert.assertThat
+import org.hamcrest.CoreMatchers.*
+import org.junit.Test
+
+class PrimeFactorsTest {
+    @Test fun factors() {
+        assertThat(factorsOf(1), `is`(emptyList()))
+        assertThat(factorsOf(2), `is`(listOf(2)))
+        assertThat(factorsOf(3), `is`(listOf(3)))
+        assertThat(factorsOf(4), `is`(listOf(2,2)))
+        assertThat(factorsOf(5), `is`(listOf(5)))
+        assertThat(factorsOf(6), `is`(listOf(2,3)))
+        assertThat(factorsOf(7), `is`(listOf(7)))
+        assertThat(factorsOf(8), `is`(listOf(2,2,2)))
+        assertThat(factorsOf(9), `is`(listOf(3,3)))
+        assertThat(factorsOf(2*2*3*3*5*7*11*11*13), `is`(listOf(2,2,3,3,5,7,11,11,13)))
+    }
+
+    private fun factorsOf(n: Int): List<Int> {
+        var remainder = n
+        val factors = mutableListOf<Int>()
+        var divisor = 2
+        while (remainder>1) {
+            while (remainder%divisor == 0) {
+                factors.add(divisor)
+                remainder /= divisor
+            }
+            divisor++
+        }
+        return factors
+    }
+}
